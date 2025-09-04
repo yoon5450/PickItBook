388e465a85272b0a82ffe8071db015fc16e1b074
YooJeong01 <ujjh77@naver.com>
2025-09-04 10:39:24 +0900

    refactor. authlayout 정리
    - 기존에 회원가입 폼 추가될 시 사용하려고 남겨뒀던 부모 컴포넌트 없애고 로그인으로 간소화했습니다

--- src/Page/Auth/Login.tsx
+++ src/Page/Auth/Login.tsx
@@ -2,14 +2,33 @@ import SocialButton from "./Components/SocialButton"
 import { memo } from "react";
 
 function Login() {
-
   return (
-    <div className="w-full flex flex-col items-center px-10 py-25">
-      <h2 className="font-semibold text-[32px] text-primary-black mb-9 text-center">Login</h2>
-      <SocialButton social='github' className={'top-39'} />
-      <SocialButton social='google' className={'top-59'} />
-
+    <div className="w-full max-w-[1200px] mx-auto min-h-[760px] justify-items-center pt-58 px-48 h-screen">
+      <h1 hidden>AuthLayout</h1>
+      <section className="flex">
+        {/* 바인더 */}
+        <div className="lg:w-[520px] md:w-sm w-2xs flex-1 border border-primary-black rounded-xl flex items-center justify-center z-20 bg-pattern">
+          <img className="w-80 px-10" src="/pickitbook_logo.svg" alt="pick it book" />
+        </div>
+        <div className="
+            relative
+            lg:w-[520px] md:w-sm w-2xs 
+            flex-1 border rounded-xl border-primary-black
+            flex items-center justify-center 
+            lg:animate-form-right-lg
+            md:animate-form-right-md
+            animate-form-right-sm
+          ">
+          {/* 바인더에 들어갈 페이지 : 로그인 폼 */}
+          <div className="w-full flex flex-col items-center px-10 py-25">
+            <h2 className="font-semibold text-[32px] text-primary-black mb-9 text-center">Login</h2>
+            <SocialButton social='github' className={'top-39'} />
+            <SocialButton social='google' className={'top-59'} />
+          </div>
+        </div>
+      </section>
     </div>
+
   )
 }
 export default memo(Login)
