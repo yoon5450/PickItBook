07582dbd0758f7a16c3d82a00f4714c919580c85
yoon5450 <dbseodnd356@naver.com>
2025-09-05 11:10:28 +0900

    [Fix] build error 수정

--- src/utils/getBookmarks.ts
+++ src/utils/getBookmarks.ts
@@ -1,7 +1,7 @@
 import type { Tables } from "@/@types/database.types";
 import supabase from "./supabase";
 
-export type BookmarkBook = Tables<"bookmark">
+export type BookmarkBook = Tables<"v_bookmark_books">
 
 export async function getBookmarks(userId: string) {
   const { data, error } = await supabase
